ee876d12f34a063f1cb3d4a48b8a42a7d56e3fa4
Michael Olatunji <tundexmike@gmail.com>
2020-12-21 20:21:06 +0100

    configuring with terraform cloud

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "remote" {
+    organization = "imyke"
+    workspaces {
+      name = "learn-terraform"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
