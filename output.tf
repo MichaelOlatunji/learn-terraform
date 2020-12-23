47e86d4c7d76a0275452d33a3db0dec03eb48769
Michael Olatunji <tundexmike@gmail.com>
2020-12-23 20:34:24 +0100

    format, adding variables

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 # output "instance_name" {
 #   value = aws_instance.name
 # }
+
+output "public_dns_name" {
+  description = "Public DNS names of the load balancer for this project"
+  value       = module.elb_http.this_elb_dns_name
+}
