6d3fb67efa4180e132448b2b2b285f894a760193
Michael Olatunji <tundexmike@gmail.com>
2020-12-23 19:56:20 +0100

    adding aws instance module

--- variables.tf
+++ variables.tf
@@ -1,5 +1,13 @@
 variable "region" {
-  type = string
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR Block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "ami" {
